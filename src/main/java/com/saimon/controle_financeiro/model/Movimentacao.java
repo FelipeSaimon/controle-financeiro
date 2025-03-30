86fc64ae4c4b8b784dc79b411293f215eda0360e
Felipe Saimon <felipesaimon.lima@gmail.com>
2025-03-30 14:12:56 -0300

    feat: recuperando codigo de usuario e movimentacao

---
+++ src/main/java/com/saimon/controle_financeiro/model/Movimentacao.java
@@ -0,0 +1,51 @@
+package com.saimon.controle_financeiro.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Movimentacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double valorMovimentacao;
+    private Date dataDeCriacao;
+    private boolean logado;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getValorMovimentacao() {
+        return valorMovimentacao;
+    }
+
+    public void setValorMovimentacao(double valorMovimentacao) {
+        this.valorMovimentacao = valorMovimentacao;
+    }
+
+    public Date getDataDeCriacao() {
+        return dataDeCriacao;
+    }
+
+    public void setDataDeCriacao(Date dataDeCriacao) {
+        this.dataDeCriacao = dataDeCriacao;
+    }
+
+    public boolean isLogado() {
+        return logado;
+    }
+
+    public void setLogado(boolean logado) {
+        this.logado = logado;
+    }
+}
