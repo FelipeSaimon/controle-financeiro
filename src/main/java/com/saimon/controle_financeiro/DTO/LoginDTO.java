dbee91e718f5502d2ed6135e583137c2dfc6a791
Felipe Saimon <felipesaimon.lima@gmail.com>
2025-04-08 19:16:10 -0300

    chore: Agrupando todos DTOs

---
+++ src/main/java/com/saimon/controle_financeiro/DTO/LoginDTO.java
@@ -0,0 +1,28 @@
+package com.saimon.controle_financeiro.DTO;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginDTO {
+
+    @NotBlank(message = "Email obrigatório")
+    private String email;
+
+    @NotBlank(message = "Senha obrigatória")
+    private String senha;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
---
+++ src/main/java/com/saimon/controle_financeiro/DTO/MovimentacaoDTO.java
@@ -0,0 +1,20 @@
+package com.saimon.controle_financeiro.DTO;
+
+import java.time.LocalDateTime;
+
+public class MovimentacaoDTO {
+    private String email;
+    private Double valorMovimentacao;
+
+    public MovimentacaoDTO(String email, double valorMovimentacao, LocalDateTime dataDeCriacao) {
+        this.valorMovimentacao = valorMovimentacao;
+        this.email = email;
+    }
+
+    // Getters e Setters
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public Double getValorMovimentacao() { return valorMovimentacao; }
+    public void setValorMovimentacao(Double valorMovimentacao) { this.valorMovimentacao = valorMovimentacao; }
+}
