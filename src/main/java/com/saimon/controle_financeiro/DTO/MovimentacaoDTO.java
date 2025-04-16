c6f96e2b16a7bb85631becb5f9bcd91196cc8ee4
Felipe Saimon <felipesaimon.lima@gmail.com>
2025-04-16 16:34:15 -0300

    raw: Enum TipoMovimentacao e BigDecimal em valorMovimentacao

--- src/main/java/com/saimon/controle_financeiro/DTO/MovimentacaoDTO.java
+++ src/main/java/com/saimon/controle_financeiro/DTO/MovimentacaoDTO.java
@@ -1,20 +1,44 @@
 package com.saimon.controle_financeiro.DTO;
 
+import com.saimon.controle_financeiro.Domain.Enum.TipoMovimentacao;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class MovimentacaoDTO {
     private String email;
-    private Double valorMovimentacao;
 
-    public MovimentacaoDTO(String email, double valorMovimentacao, LocalDateTime dataDeCriacao) {
+    @NotNull
+    @NotBlank
+    private BigDecimal valorMovimentacao;
+
+    @NotNull
+    @NotBlank
+    private TipoMovimentacao tipo;
+
+    public MovimentacaoDTO() {
+    }
+
+    public MovimentacaoDTO(String email, BigDecimal valorMovimentacao, LocalDateTime dataDeCriacao, TipoMovimentacao tipo) {
         this.valorMovimentacao = valorMovimentacao;
         this.email = email;
+        this.tipo = tipo;
     }
 
     // Getters e Setters
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 
-    public Double getValorMovimentacao() { return valorMovimentacao; }
-    public void setValorMovimentacao(Double valorMovimentacao) { this.valorMovimentacao = valorMovimentacao; }
+    public BigDecimal getValorMovimentacao() { return valorMovimentacao; }
+    public void setValorMovimentacao(BigDecimal valorMovimentacao) { this.valorMovimentacao = valorMovimentacao; }
+
+    public TipoMovimentacao getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoMovimentacao tipo) {
+        this.tipo = tipo;
+    }
 }
