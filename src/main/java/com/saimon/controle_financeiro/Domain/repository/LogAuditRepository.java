43c1eeaa6180b7dd5019b9ffb4d134888eb37a0d
Felipe Saimon <felipesaimon.lima@gmail.com>
2025-05-01 15:27:51 -0300

    remove: Removendo todo código de módulo de log

--- src/main/java/com/saimon/controle_financeiro/Domain/repository/LogAuditRepository.java
+++
@@ -1,9 +0,0 @@
-package com.saimon.controle_financeiro.Domain.repository;
-
-import com.saimon.controle_financeiro.Domain.model.LogAudit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LogAuditRepository extends JpaRepository<LogAudit, Long> {
-}
--- src/main/java/com/saimon/controle_financeiro/audit/LoggerServiceInterface.java
+++
@@ -1,8 +0,0 @@
-package com.saimon.controle_financeiro.audit;
-
-import com.saimon.controle_financeiro.Domain.Enum.LogTipoEvento;
-import com.saimon.controle_financeiro.Domain.model.Usuario;
-
-public interface LoggerServiceInterface {
-	void logEvento(Usuario usuario, LogTipoEvento evento, String descricao);
-}
